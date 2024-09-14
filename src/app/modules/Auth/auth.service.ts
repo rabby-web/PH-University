1d943726683d6b0c834d78e779c4385a6cb785b4
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-14 22:02:46 +0600

    exists user and delete user cheaking mongodb

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,6 +1,23 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 
 const loginUser = async (payload: TLoginUser) => {
+  // checking if the user exist---------
+  const isUserExists = await User.findOne({ id: payload?.id });
+  //   console.log(isUserExists);
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+  }
+
+  //  checking if the user is already deleted----------
+  const isDeleted = await isUserExists.isDeleted;
+  console.log(isUserExists);
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!');
+  }
+
   console.log(payload);
   return {};
 };
