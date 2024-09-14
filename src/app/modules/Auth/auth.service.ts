804ca349fc617a68af8c356b4729f7f7c6f7805a
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-14 22:57:28 +0600

    delete and status checking

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -6,22 +6,29 @@ import { TLoginUser } from './auth.interface';
 const loginUser = async (payload: TLoginUser) => {
   // checking if the user exist---------
   const isUserExists = await User.findOne({ id: payload?.id });
-  //   console.log(isUserExists);
+  //  console.log(isUserExists);
   if (!isUserExists) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
   }
 
   //  checking if the user is already deleted----------
-  const isDeleted = await isUserExists.isDeleted;
-  console.log(isUserExists);
+  const isDeleted = isUserExists?.isDeleted;
+//   console.log('deleted //', isDeleted);
   if (isDeleted) {
     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!');
   }
 
+  //  checking if the user is blocked----------
+  const userStatus = isUserExists?.status;
+//   console.log('status //', userStatus);
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!');
+  }
+
+  // Access Granted: Send AccessToken, RefreshToken
   console.log(payload);
   return {};
 };
-
 export const AuthService = {
   loginUser,
 };
