f56d73cccdf52e028700f2dc30a7af935a5f44f5
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-16 19:45:11 +0600

    user cahking validation jwt auth file

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,6 +5,7 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
 // interface CustomRequest extends Request {
 //   user: JwtPayload;
@@ -24,7 +25,27 @@ const auth = (...requiredRoles: TUserRole[]) => {
       config.jwt_access_secret as string,
     ) as JwtPayload;
 
-    const role = decoded.role;
+    const { role, userId, iat } = decoded;
+
+    // checking if the user exist---------
+    const user = await User.isUserExistsByCustomId(userId);
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+    }
+
+    //  checking if the user is already deleted----------
+    const isDeleted = user?.isDeleted;
+    //   console.log('deleted //', isDeleted);
+    if (isDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!');
+    }
+
+    //  checking if the user is blocked----------
+    const userStatus = user?.status;
+    //   console.log('status //', userStatus);
+    if (userStatus === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!');
+    }
 
     if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
