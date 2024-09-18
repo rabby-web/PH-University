11c95ef0669b4e88b37455825d3a7d1483e7da8b
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-18 08:13:08 +0600

    coust gurd

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -2,19 +2,25 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { CourseControllers } from './course.controller';
 import { CourseValidations } from './course.validation';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post(
   '/create-course',
+  auth('admin'),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
-router.get('/:id', CourseControllers.getSingleCourse);
+router.get(
+  '/:id',
+  auth('admin', 'student', 'faculty'),
+  CourseControllers.getSingleCourse,
+);
 
 router.patch(
-  '/:id',
+  '/:id', auth('admin'),
   validateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
@@ -23,12 +29,14 @@ router.delete('/:id', CourseControllers.deleteCourse);
 
 router.put(
   '/:courseId/assign-faculties',
+  auth('admin'),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.assignFacultiesWithCourse,
 );
 
 router.delete(
   '/:courseId/remove-faculties',
+  auth('admin'),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.removeFacultiesFromCourse,
 );
